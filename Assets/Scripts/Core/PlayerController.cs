d7f6b6e8b2e89908f4494e4f9eced14deba538c2
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2022-10-12 16:33:16 +0300

    -Added new follow camera -Added horizontal clamping

--- Assets/Scripts/Core/PlayerController.cs
+++ Assets/Scripts/Core/PlayerController.cs
@@ -1,5 +1,5 @@
-using System;
 using ShotMergerClone.Managers;
+using System;
 using UnityEngine;
 
 namespace ShotMergerClone.Core
@@ -9,9 +9,13 @@ namespace ShotMergerClone.Core
         [SerializeField] private GameObject bulletGO;
         [SerializeField] private Transform bulletSpawnTransform;
         [SerializeField] private float spawnDelay = 2f;
-        [SerializeField] private float movementSpeed = 10f;
+        [SerializeField] private float forwardSpeed = 10f;
+        [SerializeField] private float horizontalSpeed = 10f;
 
+        private float minXClamp = -2.07f;
+        private float maxXClamp = 2.083f;
         private float innerSpawnDelay;
+
         private Action playerState;
 
         private void Update()
@@ -27,11 +31,13 @@ namespace ShotMergerClone.Core
 
         private void Move()
         {
-            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime, Space.World);
 
             if (Input.GetMouseButton(0))
             {
-                transform.Translate(new Vector3(0, -InputManager.Delta.x, 0));
+                transform.Translate(new Vector3(0, -InputManager.Delta.x, 0) * horizontalSpeed);
+                transform.position = new Vector3(Mathf.Clamp(transform.position.x, minXClamp, maxXClamp),
+                    transform.position.y, transform.position.z);
             }
         }
 
@@ -41,7 +47,7 @@ namespace ShotMergerClone.Core
 
             if (innerSpawnDelay <= 0)
             {
-                var bullet = Instantiate(bulletGO, bulletSpawnTransform.position, bulletGO.transform.rotation);
+                Instantiate(bulletGO, bulletSpawnTransform.position, bulletGO.transform.rotation);
                 innerSpawnDelay = spawnDelay;
             }
         }
